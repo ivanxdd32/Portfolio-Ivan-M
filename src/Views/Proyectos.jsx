8e44f8f31885d365dc5078fa1c11d47ff6ae34ca
ivanxdd32 <ivanandresmartinezrios@gmail.com>
2025-03-19 14:48:13 -0500

    an animation was added to the title

--- src/Views/Proyectos.jsx
+++ src/Views/Proyectos.jsx
@@ -1,16 +1,38 @@
 import React, { useRef } from 'react';
-import { motion } from "framer-motion";
+import { delay, motion } from "framer-motion";
 
 export default function Projectos() {
+
+    const textoAnimado = "Mis Proyectos";
+
+    // Animación para cada letra
+    const letterVariants = {
+      hidden: { opacity: 0, y: -10 }, // Inicia con opacidad 0 y un poco arriba
+      visible: (i) => ({
+        opacity: 1,
+        y: 0, // Baja a su posición normal
+        transition: { duration: 0.05, delay: 0.5 + i * 0.05 }, // Efecto ola
+      }),
+    };
+  
+
   return (
     <section className="h-screen text-center font-sans bg-gray-900 text-white py-20">
       <motion.h2
         className="text-3xl font-bold text-center pt-4"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.5 }}
+          initial="hidden"
+          animate="visible"
       >
-        Mis Proyectos
+        {textoAnimado.split("").map((letra, i) => (
+          <motion.span
+                key={i}
+                className="inline-block"
+                variants={letterVariants}
+                custom={i} // Pasa el índice para calcular el delay
+              >
+                {letra === " " ? "\u00A0" : letra} {/* Mantiene los espacios */}
+              </motion.span>
+          ))}
       </motion.h2>
       <div className="grid grid-cols-1 gap-6 mt-8 py-4 px-6 sm:px-10 md:px-16 lg:px-20">
         <motion.div
