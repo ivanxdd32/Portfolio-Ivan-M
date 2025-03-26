b4efb95b285ae896406dcc8b1f84699ec2422d5f
ivanxdd32 <ivanandresmartinezrios@gmail.com>
2025-03-25 21:19:43 -0500

    Corrigiendo redirecciones en Render

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // Asegura que la salida es dist
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
+  },
+  // Copiar _redirects a dist/
+  publicDir: 'public',
 })
